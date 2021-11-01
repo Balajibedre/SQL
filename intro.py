93fde6d97fa94aaa93b0c8c8cfba4f5ac594067d
BALAJI BEDRE <89333765+Balajibedre@users.noreply.github.com>
2021-11-01 11:18:54 +0530

    Add files via upload
    add

---
+++ intro.py
@@ -0,0 +1,31 @@
+## What is SQL?
+
+
+SQL stands for Structured Query Language
+SQL lets you access and manipulate databases
+SQL became a standard of the American National Standards Institute (ANSI) in 1986, 
+and of the International Organization for Standardization (ISO) in 1987
+
+#--------------------------------------------------------------------------
+
+SQL is a Standard - BUT....
+
+
+Although SQL is an ANSI/ISO standard, there are different versions of the SQL language.
+
+However, to be compliant with the ANSI standard, they all support at least the major commands 
+(such as SELECT, UPDATE, DELETE, INSERT, WHERE) in a similar manner.
+
+#----------------------------------------------------------------------------
+
+The SQL SELECT Statement
+
+
+The SELECT statement is used to select data from a database.
+
+The data returned is stored in a result table, called the result-set.
+
+SELECT Syntax
+
+SELECT column1, column2, ...
+FROM table_name;
